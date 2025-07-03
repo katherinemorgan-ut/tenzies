01e6cd110017f201a2604c0dda57671357ca515d
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-07-03 10:43:04 -0600

    tenzies: add endgame & restart functionality

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Die from './components/Die.jsx'
 import RollButton from './components/RollButton.jsx';
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti'
 
 function App() {
-  
+
   function getDiceObjects() {
     let values = new Array(10).fill(1);
     let valuesObject = values.map(x => ({
@@ -24,7 +25,12 @@ function App() {
   }
 
 
-  let [ dieObjectArray, setdieObjectArray ] = useState(getDiceObjects())
+  let [ dieObjectArray, setdieObjectArray ] = useState(() => getDiceObjects())
+  const endgameButtonRef = useRef(null);
+
+  let gameWon = 
+    dieObjectArray.every( die => die.isHeld) &&
+    dieObjectArray.every( die => die.value === dieObjectArray[0].value);
 
   let diceElements = dieObjectArray.map( dieObject => < Die 
     key={dieObject.id} 
@@ -36,6 +42,12 @@ function App() {
 
   
   function rollDice() {
+    // If user has won the game, reset the dice & the gameWon variable
+    if(gameWon) {
+      setdieObjectArray(getDiceObjects());
+      gameWon = false;
+    }
+    else {
     setdieObjectArray( prevDieObjectArray => prevDieObjectArray.map( 
       die => (
         die.isHeld == true ?
@@ -43,16 +55,30 @@ function App() {
         {...die, value: Math.floor(Math.random()*7)})
     ))
   }
+  }
+
+  useEffect( () => {
+    if (gameWon) {
+      endgameButtonRef.current.focus();
+    }
+
+  }, [gameWon])
 
   return (
     <main>
+      { gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+          {gameWon && <p>Congratulations! You won. Press "New Game" to play again.</p>}
+      </div>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="flex-container">
         {diceElements}
       </div>
       <br></br>
       <br></br>
       <br></br>
-      < RollButton onClick={rollDice} className="roll-dice" />
+      < RollButton onClick={rollDice} className="roll-dice" gameWon={gameWon} buttonRef={endgameButtonRef}/>
     </main>
   )
 }
--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -4,7 +4,13 @@ function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
-    return <button style={styles} onClick={() => props.holdFunction(props.id)} >
+    return <button 
+    style={styles} 
+    onClick={() => props.holdFunction(props.id)} 
+    aria-label={`Die with a value of ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+    aria-pressed={props.isHeld}
+    
+    >
         {props.dieValue}
     </button>
 }
